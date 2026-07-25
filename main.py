3f8d6b8c01d859d2a9e1953d8225948910a4b76d
chiru1526 <dvav.chiranjeevi@gmail.com>
2026-07-25 19:44:47 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+list=[27,41,82,94,79,56,38]
+print("List=",list)
+ans=0
+for i in list:
+    ans=ans+i
+print("Sum=",ans)
+print("Average=",ans/len(list))
+print("Lowest number=",list[0])
+print("Highest number=",list[-1])
